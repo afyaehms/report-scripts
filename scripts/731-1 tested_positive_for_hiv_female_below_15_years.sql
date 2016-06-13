06bc811e73e22070669529784ef1e2ef8ee2b7f0
Dennys Henry <daugmaud@gmail.com>
2016-06-13 13:22:34 +0300

    Completed 731-1 scripts

---
+++ scripts/731-1 tested_positive_for_hiv_female_below_15_years.sql
@@ -0,0 +1,13 @@
+--get count, people that Tested postive for HIV under 5yrs
+--substitute the dates with date placeholders
+
+SELECT COUNT(*)
+FROM obs o
+INNER JOIN person p ON p.person_id=o.person_id
+
+WHERE (concept_id=3110 OR concept_id=100126141)
+AND value_text LIKE 'POSITIVE'
+AND p.birthdate IS NOT NULL 
+AND FLOOR(DATEDIFF(CURDATE(),p.birthdate) / 365) <15
+AND gender = 'F'
+AND o.date_created BETWEEN '2016-01-01' AND '2016-06-30'
