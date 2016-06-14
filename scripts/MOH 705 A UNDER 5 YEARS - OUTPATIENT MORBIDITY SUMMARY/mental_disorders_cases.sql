3b5aa42f4e3493b88f37f065867a9797ca858ae5
Geof Wasilwa <geoffreywanjala@gmail.com>
2016-06-14 14:44:21 +0300

    Add mental disorder cases script

---
+++ scripts/MOH 705 A UNDER 5 YEARS - OUTPATIENT MORBIDITY SUMMARY/mental_disorders_cases.sql
@@ -0,0 +1,17 @@
+-- Get count of new medical disorder cases
+SELECT COUNT(DISTINCT p.person_id) AS 'Count'
+FROM person p
+INNER JOIN obs o ON o.person_id = p.person_id
+WHERE
+p.voided = 0
+AND o.voided = 0
+AND o.concept_id=5109
+AND DATE(o.obs_datetime) BETWEEN :startOfPeriod AND :endOfPeriod
+AND o.value_coded IN (2044,1845,3659,3660,4586,134337,160200)
+AND EXTRACT(YEAR FROM (FROM_DAYS(DATEDIFF(NOW(),p.birthdate)))) &lt;= 5 
+-- get only new cases
+AND p.person_id NOT IN (
+  SELECT person_id FROM obs 
+  WHERE concept_id = 5109 
+  AND value_coded IN (2044,1845,3659,3660,4586,134337,160200)
+  AND DATE(obs_datetime) &lt; :startOfPeriod)
