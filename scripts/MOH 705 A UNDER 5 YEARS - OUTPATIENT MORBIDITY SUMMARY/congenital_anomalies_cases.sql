fd5f813d941cccc97596e0586e39ee45826f8da4
Geof Wasilwa <geoffreywanjala@gmail.com>
2016-06-14 15:33:39 +0300

    Change value coded values for congenital anomaly cases

--- scripts/MOH 705 A UNDER 5 YEARS - OUTPATIENT MORBIDITY SUMMARY/congenital_anomalies_cases.sql
+++ scripts/MOH 705 A UNDER 5 YEARS - OUTPATIENT MORBIDITY SUMMARY/congenital_anomalies_cases.sql
@@ -7,13 +7,11 @@ p.voided = 0
 AND o.voided = 0
 AND o.concept_id=5109
 AND DATE(o.obs_datetime) BETWEEN :startOfPeriod AND :endOfPeriod
-AND o.value_coded IN
-  (1155,1825,1828,1934,1138,4949,137293,150555,512,160161,137293)
+AND o.value_coded IN (1025,1201)
 AND EXTRACT(YEAR FROM (FROM_DAYS(DATEDIFF(NOW(),p.birthdate)))) &lt;= 5
 -- get only new cases
 AND p.person_id NOT IN (
   SELECT person_id FROM obs 
   WHERE concept_id = 5109 
-  AND value_coded IN
-    (1155,1825,1828,1934,1138,4949,137293,150555,512,160161,137293)
+  AND value_coded IN (1025,1201)
   AND DATE(obs_datetime) &lt; :startOfPeriod)
