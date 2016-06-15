f73b888cf0dd18c36469c8733bc6f429124bafaf
Dennys Henry <daugmaud@gmail.com>
2016-06-15 17:55:56 +0300

    Service workload and Laboratory Summary

---
+++ scripts/MOH 706 Laboratory Summary/2. Blood Chemistry/blood_chemistry_blood_ogtt_high.sql
@@ -0,0 +1,27 @@
+--OGGT represents Glucose Blood Tolerance (concept_id: 2404)
+--It composes of 4 tests, and comment field field
+--We are getting the value of the four test and ignoring the comment field (concept_id: 5128)
+--The High and Low Limits are joined from the concept_numeric table
+--All the four tests are group as 1 test and assumption made that if any of them is greater than it limit, then thats a positive as over
+--Substitute the dates with date placeholders
+
+SELECT COUNT(*) 
+FROM(
+	SELECT obs.encounter_id,
+	obs.person_id
+
+	FROM obs
+	INNER JOIN orders o 
+	ON obs.order_id=o.order_id
+	AND o.concept_id=2404
+
+	LEFT OUTER JOIN concept_numeric c
+	ON obs.concept_id = c.concept_id
+
+	WHERE obs.concept_id<>5128
+	AND DATE(obs_datetime) BETWEEN '2016-01-01' AND '2016-06-31'
+
+
+	GROUP BY obs.person_id, obs.encounter_id
+	HAVING MAX(CASE WHEN value_numeric>=hi_normal THEN 1 ELSE 0 END)=1
+) AS oggt_high
---
+++ scripts/MOH 706 Laboratory Summary/2. Blood Chemistry/blood_chemistry_blood_sugar_test_low.sql
@@ -0,0 +1,12 @@
+--Blood Sugar test are two: 
+--Random Blood Sugar (concept_id: 2223) has a high of 7.8 and a low of 2.5
+--Fasting Blood Sugar (concept_id: 2291) has a high of 6.1 and a low of 2.0
+--I am combining both and testing using the different Levels
+--Substitute the dates with date placeholders
+
+SELECT COUNT(*)
+FROM obs
+
+WHERE obs.concept_id=2223
+AND value_numeric <= 2.5
+AND DATE(obs_datetime) BETWEEN '2016-01-01' AND '2016-06-30';
