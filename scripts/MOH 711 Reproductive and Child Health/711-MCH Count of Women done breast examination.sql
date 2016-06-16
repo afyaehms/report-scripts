663db9a5c551457b89ccfb036490d06813dd30aa
stansL <mstans8@gmail.com>
2016-06-16 13:12:25 +0300

    add script Total women done breast examination

--- scripts/MOH 711 Reproductive and Child Health/711-MCH Count of Women done breast examination.sql
+++ scripts/MOH 711 Reproductive and Child Health/711-MCH Count of Women done breast examination.sql
@@ -1,3 +1,10 @@
---get count of Mothers done breast examination
+--get Total women done breast examination
 --substitute the dates with date placeholders
 --substitute the program_id with the appropriate program id(1=>ANC, 2=>PNC)
+
+SELECT COUNT(*)
+FROM obs o
+INNER JOIN patient_program pp ON pp.patient_id = o.person_id
+WHERE o.concept_id = 5538
+AND (pp.program_id = 1 OR pp.program_id = 2)
+AND o.obs_datetime BETWEEN '2016-06-14' AND '2016-06-17'
