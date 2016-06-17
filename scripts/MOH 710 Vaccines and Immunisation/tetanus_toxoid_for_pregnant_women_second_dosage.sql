5cb7e69801f38a062af6d3ffb48eba48b35c562c
franqq <githae.francis@gmail.com>
2016-06-17 10:40:12 +0300

    Second dosage

---
+++ scripts/MOH 710 Vaccines and Immunisation/tetanus_toxoid_for_pregnant_women_second_dosage.sql
@@ -0,0 +1,14 @@
+--Tetanus toxoid for pregnant women
+--Second Dosage
+--Substitute the dates with date placeholders
+
+SELECT COUNT(*)
+ 
+FROM patient_state ps
+INNER JOIN patient_program pg ON pg.patient_program_id=ps.patient_program_id
+INNER JOIN person p ON pg.patient_id = p.person_id
+INNER JOIN program_workflow_state pws ON pws.program_workflow_state_id=ps.state
+ 
+WHERE  pws.concept_id = 100126175
+AND start_date BETWEEN '2016-01-01' AND '2016-06-30'
+
