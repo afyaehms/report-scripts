acf8fabd778e0712390ea376a0d3d3a6d2078608
franqq <githae.francis@gmail.com>
2016-06-17 08:31:30 +0300

    Measles at two and above years

--- scripts/MOH 710 Vaccines and Immunisation/measles_at_two_and_above_years.sql
+++ scripts/MOH 710 Vaccines and Immunisation/measles_at_two_and_above_years.sql
@@ -11,4 +11,4 @@ INNER JOIN program_workflow_state pws ON pws.program_workflow_state_id=ps.state
 WHERE  pws.concept_id = 100126193 OR pws.concept_id = 100126194 OR pws.concept_id = 100126195
 AND start_date BETWEEN '2016-01-01' AND '2016-06-30'
 AND birthdate IS NOT NULL 
-AND ((DATEDIFF(CURDATE(),birthdate) / 365) >= 1.5 OR (DATEDIFF(CURDATE(),birthdate) / 365) < 2)
+AND (DATEDIFF(CURDATE(),birthdate) / 365) >= 2
--- scripts/MOH 710 Vaccines and Immunisation/vitamin_A_at_1_years_(200,000IU).sql
+++ scripts/MOH 710 Vaccines and Immunisation/vitamin_A_at_1_years_(200,000IU).sql
@@ -1,5 +1,5 @@
 --Vitamin A at 1years (200,000IU)
---Patients exactlly 1 year old
+--Patients above 1 year and below 11/2 years old
 --Substitute the dates with date placeholders
 
 SELECT COUNT(*)
@@ -12,4 +12,4 @@ INNER JOIN program_workflow_state pws ON pws.program_workflow_state_id=ps.state
 WHERE  pws.concept_id = 100126165
 AND start_date BETWEEN '2016-01-01' AND '2016-06-30'
 AND birthdate IS NOT NULL 
-AND (DATEDIFF(CURDATE(),birthdate) / 365) = 1
+AND (DATEDIFF(CURDATE(),birthdate) / 365) <= 1.5
