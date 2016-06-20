88f921649b68eabadffcaf000eebfe17d169caef
franqq <githae.francis@gmail.com>
2016-06-20 16:25:20 +0300

    Bcg doses remaining at the end of the month

--- scripts/MOH 710 Vaccines and Immunisation/bcg_doses_remaining_at_the_end_of_the_month.sql
+++ scripts/MOH 710 Vaccines and Immunisation/bcg_doses_remaining_at_the_end_of_the_month.sql
@@ -1,9 +1,9 @@
--- BCG doses in stock at the beginning of the month
+-- BCG remaining at the end of the month
 -- Substitute the dates with date placeholders
 
-SELECT current_quantity
+SELECT closing_balance
 FROM drug dr
 INNER JOIN inventory_store_drug isd ON isd.drug_id = dr.drug_id
-WHERE concept_id= 193 AND created_on <= '2016-06-01'
+WHERE concept_id= 193 AND created_on <= '2016-06-30'
 ORDER BY created_on DESC
 LIMIT 1;
