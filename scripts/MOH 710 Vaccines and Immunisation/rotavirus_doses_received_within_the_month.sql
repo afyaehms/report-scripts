5dd0039700242bfd19d589b9bc127ca5dad0e236
franqq <githae.francis@gmail.com>
2016-06-20 17:18:45 +0300

    Rotavirus doses reveived within the month

---
+++ scripts/MOH 710 Vaccines and Immunisation/rotavirus_doses_received_within_the_month.sql
@@ -0,0 +1,7 @@
+-- BCG received within the month
+-- Substitute the dates with date placeholders
+
+SELECT SUM(receipt_quantity)
+FROM drug dr
+INNER JOIN inventory_store_drug isd ON isd.drug_id = dr.drug_id
+WHERE concept_id=83533 AND (created_on BETWEEN '2016-06-01' AND '2016-06-30');
