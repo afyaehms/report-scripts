df90ee0203d2879e0093cbb1a049979de84fe44d
franqq <githae.francis@gmail.com>
2016-06-20 17:52:05 +0300

    Vitamin A 200000 doses in stock at the beggining of the month

---
+++ scripts/MOH 710 Vaccines and Immunisation/vitamin_A_200000_doses_in_stock_at_the_beggining_of_the_month.sql
@@ -0,0 +1,9 @@
+-- Vitamin A 200,000 doses in stock at the beginning of the month
+-- Substitute the dates with date placeholders
+
+SELECT current_quantity
+FROM drug dr
+INNER JOIN inventory_store_drug isd ON isd.drug_id = dr.drug_id
+WHERE concept_id= 100126165 AND created_on <= '2016-06-01'
+ORDER BY created_on DESC
+LIMIT 1;
