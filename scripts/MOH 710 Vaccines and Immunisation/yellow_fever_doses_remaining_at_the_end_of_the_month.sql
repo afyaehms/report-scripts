815751906b32f48336ea03ca723511d7ad404fb2
franqq <githae.francis@gmail.com>
2016-06-20 17:34:30 +0300

    Yellow fever doses remaining at the end of the month

---
+++ scripts/MOH 710 Vaccines and Immunisation/yellow_fever_doses_remaining_at_the_end_of_the_month.sql
@@ -0,0 +1,9 @@
+-- Yellow fever remaining at the end of the month
+-- Substitute the dates with date placeholders
+
+SELECT closing_balance
+FROM drug dr
+INNER JOIN inventory_store_drug isd ON isd.drug_id = dr.drug_id
+WHERE concept_id= 1018 AND created_on <= '2016-06-30'
+ORDER BY created_on DESC
+LIMIT 1;
