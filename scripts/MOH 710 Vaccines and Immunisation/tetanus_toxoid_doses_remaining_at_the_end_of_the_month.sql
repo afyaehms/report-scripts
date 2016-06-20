c1aa473473378d3afb680cd769ba282b75f572d8
franqq <githae.francis@gmail.com>
2016-06-20 17:41:59 +0300

    Tetanus toxoid doses remaining at the end of the month

---
+++ scripts/MOH 710 Vaccines and Immunisation/tetanus_toxoid_doses_remaining_at_the_end_of_the_month.sql
@@ -0,0 +1,9 @@
+-- Tetanus Toxoid remaining at the end of the month
+-- Substitute the dates with date placeholders
+
+SELECT closing_balance
+FROM drug dr
+INNER JOIN inventory_store_drug isd ON isd.drug_id = dr.drug_id
+WHERE concept_id= 88 AND created_on <= '2016-06-30'
+ORDER BY created_on DESC
+LIMIT 1;
