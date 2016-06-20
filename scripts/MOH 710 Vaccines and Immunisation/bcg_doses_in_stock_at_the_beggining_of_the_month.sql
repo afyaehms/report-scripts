b5593b95cbbb6269c2df79463f01688c3c639a03
franqq <githae.francis@gmail.com>
2016-06-20 16:13:03 +0300

    Bcg doses in stock at the beggining of the month

--- scripts/MOH 710 Vaccines and Immunisation/bcg_doses_in_stock_at_the_beggining_of_the_month.sql
+++ scripts/MOH 710 Vaccines and Immunisation/bcg_doses_in_stock_at_the_beggining_of_the_month.sql
@@ -4,6 +4,7 @@
 SELECT current_quantity
 FROM drug dr
 INNER JOIN inventory_store_drug isd ON isd.drug_id = dr.drug_id
-WHERE concept_id= 193 AND created_on <= '2016-06-01'
+WHERE concept_id= 193 AND created_on &lt;= '2016-06-01'
+>>>>>>> Bcg doses in stock at the beggining of the month
 ORDER BY created_on DESC
 LIMIT 1;
