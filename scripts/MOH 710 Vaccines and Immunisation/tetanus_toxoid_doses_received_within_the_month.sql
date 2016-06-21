5c2764632b99f4e97a14f565a7ccb3e45891f207
Dennys Henry <daugmaud@gmail.com>
2016-06-21 15:13:56 +0300

    Fixed 710 vaccine counts

--- scripts/MOH 710 Vaccines and Immunisation/tetanus_toxoid_doses_received_within_the_month.sql
+++ scripts/MOH 710 Vaccines and Immunisation/tetanus_toxoid_doses_received_within_the_month.sql
@@ -1,7 +1,7 @@
 -- Tetanus Toxoid received within the month
 -- Substitute the dates with date placeholders
 
-SELECT SUM(receipt_quantity)
+SELECT IFNULL(SUM(receipt_quantity),0)
 FROM drug dr
 INNER JOIN inventory_store_drug isd ON isd.drug_id = dr.drug_id
 WHERE concept_id=88 AND (created_on BETWEEN '2016-06-01' AND '2016-06-30');
--- scripts/MOH 710 Vaccines and Immunisation/vitamin_A_200000_doses_received_within_the_month.sql
+++ scripts/MOH 710 Vaccines and Immunisation/vitamin_A_200000_doses_received_within_the_month.sql
@@ -1,7 +1,7 @@
 -- Vitamin A 200000 received within the month
 -- Substitute the dates with date placeholders
 
-SELECT SUM(receipt_quantity)
+SELECT IFNULL(SUM(receipt_quantity),0)
 FROM drug dr
 INNER JOIN inventory_store_drug isd ON isd.drug_id = dr.drug_id
 WHERE concept_id=100126165 AND (created_on BETWEEN '2016-06-01' AND '2016-06-30');
