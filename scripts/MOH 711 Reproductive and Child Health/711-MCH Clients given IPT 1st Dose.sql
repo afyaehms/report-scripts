e1a578ef8408f50e530c46823a016b0fc8656572
muchemicarol <cmcayo4@gmail.com>
2016-06-22 13:24:27 +0300

    Rename MOH 711 scripts

--- scripts/MOH 711 Reproductive and Child Health/711-MCH Clients given IPT 1st Dose.sql
+++
@@ -1,7 +0,0 @@
---get count of Clients given IPT 1st Dose
---substitute the begin and end dates with date placeholders
---State id for MALARIA PROPHYLAXIS 1PT1 is fixed at 25
-SELECT count(*)
-FROM patient_state
-WHERE state = '25'
-AND start_date BETWEEN '2012-05-12' and '2016-06-13';
