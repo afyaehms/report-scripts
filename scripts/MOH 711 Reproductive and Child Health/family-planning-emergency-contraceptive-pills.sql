e213adaa77984a23bd8f1fb888de152ea438f68b
franqq <githae.francis@gmail.com>
2016-06-22 18:17:01 +0300

    Family planning

---
+++ scripts/MOH 711 Reproductive and Child Health/family-planning-emergency-contraceptive-pills.sql
@@ -0,0 +1,11 @@
+-- Emergency contraceptive pills
+-- Substitute the dates with date placeholdersSELECT COUNT(*)
+
+FROM patient_state ps
+INNER JOIN patient_program pg ON pg.patient_program_id=ps.patient_program_id
+INNER JOIN person p ON pg.patient_id = p.person_id
+INNER JOIN program_workflow_state pws ON pws.program_workflow_state_id=ps.state
+
+WHERE pws.concept_id = 160570
+AND start_date BETWEEN '2016-01-01' AND '2016-06-30'
+
