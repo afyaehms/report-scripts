a736d7fec74cd122530f81bd41369ef7e29f80b0
stansL <mstans8@gmail.com>
2016-06-22 15:30:59 +0300

    clean 731-1

--- scripts/MOH 731-1 HIV Counselling And Testing/testing-for-hiv-repeat.sql
+++ scripts/MOH 731-1 HIV Counselling And Testing/testing-for-hiv-repeat.sql
@@ -15,4 +15,4 @@ LEFT OUTER JOIN
 
 WHERE (concept_id=3110 OR concept_id=100126141)
 AND date_created BETWEEN '2016-06-01' AND '2016-06-30'
-AND ob.first_time_idnt &lt; &gt; o.obs_id
+AND ob.first_time_idnt &lt;&gt; o.obs_id
