57eee82d075f15043f6f07af724b925cb4dbe08a
kimani-njoroge <kimani064@gmail.com>
2016-06-23 14:57:10 +0300

    age/weight status

---
+++ scripts/MOH 711 Reproductive and Child Health/Count-of-normal-weight-status-babies-between-ages-24mnths-and-59mnths.sql
@@ -0,0 +1,13 @@
+--get count of normal weight babies aged 24mnths-59mnths
+--substitute the dates with date placeholders
+--substitute the program_id WITH the appropriate program id(1=>ANC, 2=>PNC, 3=>CWC
+SELECT COUNT(*)
+FROM obs o
+INNER JOIN patient_program pp ON pp.patient_id = o.person_id
+INNER JOIN person p ON pp.patient_id = p.person_id
+WHERE o.concept_id = 1854 AND value_coded = '831'
+AND pp.program_id = 3
+AND o.date_created BETWEEN '2016-06-17' AND '2016-06-18'
+AND p.birthdate IS NOT NULL
+AND (DATEDIFF(CURDATE(),p.birthdate)) / 365 >= 2
+AND (DATEDIFF(CURDATE(),p.birthdate)) / 365 <= 5
