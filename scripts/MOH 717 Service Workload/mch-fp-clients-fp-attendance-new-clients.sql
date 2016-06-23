043a3847486585050732073168c440d944ff3704
Dennys Henry <daugmaud@gmail.com>
2016-06-23 11:44:46 +0300

    717 mch-fp-attan

--- scripts/MOH 717 Service Workload/mch-fp-clients-fp-attendance-new-clients.sql
+++ scripts/MOH 717 Service Workload/mch-fp-clients-fp-attendance-new-clients.sql
@@ -1,2 +1,18 @@
---this will be handled when the family planning module has been introduced
---At that point is when it will be merged with those from PNC
+-- this will be handled when the family planning module has been introduced
+-- for now we are pulling only from PNC
+-- at that point is when it will be merged with those from PNC
+-- substitute the dates with date placeholders
+-- concepts 374 represent Method of family planning
+-- This gets the first time they were reviewed, and count grouping per patient within the selected period
+-- Substitute the dates with date placeholders
+
+
+SELECT COUNT(*)
+FROM (
+	SELECT MIN(DATE(obs_datetime)) fp_regdate
+	FROM obs o
+	WHERE concept_id = 374
+	GROUP BY person_id
+	ORDER BY obs_datetime ASC
+) AS fp
+WHERE fp_regdate BETWEEN '2016-06-01' AND '2016-06-30';
--- scripts/MOH 717 Service Workload/mch-fp-clients-fp-attendance-revisits.sql
+++ scripts/MOH 717 Service Workload/mch-fp-clients-fp-attendance-revisits.sql
@@ -1,2 +1,24 @@
---this will be handled when the family planning module has been introduced
---At that point is when it will be merged with those from PNC
+-- this will be handled when the family planning module has been introduced
+-- for now we are pulling only from PNC
+-- at that point is when it will be merged with those from PNC
+-- substitute the dates with date placeholders
+-- concepts 374 represent Method of family planning
+-- This gets the times that they were reviewed thats not the first time.
+-- Substitute the dates with date placeholders
+
+
+SELECT COUNT(*)
+FROM (
+	SELECT person_id, DATE(obs_datetime) obs_date
+	FROM obs o
+	WHERE concept_id = 374
+	AND CONCAT(DATE(obs_datetime),'-',person_id) NOT IN (
+		SELECT CONCAT(MIN(DATE(obs_datetime)),'-',person_id) fp_regdate
+		FROM obs o
+		WHERE concept_id = 374
+		GROUP BY person_id
+		ORDER BY obs_datetime ASC
+	)
+	GROUP BY person_id, DATE(obs_datetime)
+) AS z1
+WHERE obs_date BETWEEN '2016-06-01' AND '2016-06-30'
