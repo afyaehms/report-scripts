2dca172490334dca3d4ac8b0a8e4265a8c6af58c
Dennys Henry <daugmaud@gmail.com>
2016-06-23 10:42:14 +0300

    separate IPV doses from OPV doses

--- scripts/MOH 710 Vaccines and Immunisation/ipv-doses-in-stock-at-the-beggining-of-the-month.sql
+++ scripts/MOH 710 Vaccines and Immunisation/ipv-doses-in-stock-at-the-beggining-of-the-month.sql
@@ -1,13 +1,13 @@
--- OPV remaining at the end of the month
+-- IPV doses in stock at the beginning of the month
 -- Substitute the dates with date placeholders
 
 SELECT IFNULL(SUM(current_quantity),0)
 FROM(
-	SELECT IFNULL(SUM(closing_balance),0)
+	SELECT current_quantity
 	FROM drug dr
 	INNER JOIN inventory_store_drug isd ON isd.drug_id = dr.drug_id
-	WHERE (concept_id= 100126153 OR concept_id=100126150 OR concept_id=100126151) 
-	AND created_on &lt;= '2016-06-30'
+	WHERE concept_id = 100126152  
+	AND created_on &lt; '2016-06-01'
 	ORDER BY created_on DESC
 	LIMIT 1
 ) AS qnty;
--- scripts/MOH 710 Vaccines and Immunisation/opv-doses-in-stock-at-the-beggining-of-the-month.sql
+++ scripts/MOH 710 Vaccines and Immunisation/opv-doses-in-stock-at-the-beggining-of-the-month.sql
@@ -6,7 +6,7 @@ FROM(
 	SELECT current_quantity
 	FROM drug dr
 	INNER JOIN inventory_store_drug isd ON isd.drug_id = dr.drug_id
-	WHERE (concept_id= 100126153 OR concept_id= 100126150 OR concept_id= 100126151 OR concept_id= 100126152)  
+	WHERE (concept_id= 100126153 OR concept_id= 100126150 OR concept_id= 100126151)  
 	AND created_on &lt; '2016-06-01'
 	ORDER BY created_on DESC
 	LIMIT 1
