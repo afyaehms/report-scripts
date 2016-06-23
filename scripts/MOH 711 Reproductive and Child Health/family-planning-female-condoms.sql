b4bec9e4819b2981348e782485cee0562d08435e
franqq <githae.francis@gmail.com>
2016-06-23 17:09:49 +0300

    Fp methods finalized

---
+++ scripts/MOH 711 Reproductive and Child Health/family-planning-female-condoms.sql
@@ -0,0 +1,15 @@
+-- Clients receiving female condoms
+-- Substitute the dates with date placeholders
+
+SELECT COUNT(*)
+
+FROM patient_state ps
+INNER JOIN patient_program pg ON pg.patient_program_id=ps.patient_program_id
+INNER JOIN person p ON pg.patient_id = p.person_id
+INNER JOIN program_workflow_state pws ON pws.program_workflow_state_id=ps.state
+
+WHERE pws.concept_id = 190 AND p.gender = 'F'
+AND start_date BETWEEN '2016-01-01' AND '2016-06-30'
+
+
+
