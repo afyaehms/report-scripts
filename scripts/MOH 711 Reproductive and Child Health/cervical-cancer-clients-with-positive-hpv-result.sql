bea1fdba6f656761f8c209772ea53c0a75e6d098
stansL <mstans8@gmail.com>
2016-06-24 16:23:20 +0300

    Cervical cancer clients with Positive HPV result

---
+++ scripts/MOH 711 Reproductive and Child Health/cervical-cancer-clients-with-positive-hpv-result.sql
@@ -0,0 +1,10 @@
+--get count of Cervical cancer clients screened for HPV test
+--substitute the dates with date placeholders
+--100126199 - placeholder id for HPV TEST - to be replaced once included as an investigation
+
+SELECT COUNT(*)
+FROM obs o
+INNER JOIN person p ON p.person_id=o.person_id
+WHERE concept_id=100126199
+AND value_coded = 782
+AND o.date_created BETWEEN '2016-06-01' AND '2016-06-30'
