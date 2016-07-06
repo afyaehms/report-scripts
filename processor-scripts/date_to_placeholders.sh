3d25990bb1343e07f2d3154d11eb5740245199ad
Geof Wasilwa <geoffreywanjala@gmail.com>
2016-07-06 22:35:38 +0300

    Update date to placeholder script to be case insensitive

--- processor-scripts/date_to_placeholders.sh
+++ processor-scripts/date_to_placeholders.sh
@@ -4,8 +4,8 @@ FILE=$1
 filename=$(basename "$FILE")
 
 sed "{ 
-  s|\('[0-9][0-9][0-9][0-9]-[0-9][0-9]*-[0-9][0-9]*'\)\(\sAND\)|:startOfPeriod\2|
-  s|\(AND\s\)\('[0-9][0-9][0-9][0-9]-[0-9][0-9]*-[0-9][0-9]*'\)|\1:endOfPeriod|
+  s|\('[0-9][0-9][0-9][0-9]-[0-9][0-9]*-[0-9][0-9]*'\)\(\sAND\)|:startOfPeriod\2|I
+  s|\(AND\s\)\('[0-9][0-9][0-9][0-9]-[0-9][0-9]*-[0-9][0-9]*'\)|\1:endOfPeriod|I
 }" "$FILE"  > "compiled/$filename"
 
 echo "$filename"
