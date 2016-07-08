cdd2ded0f8a6bb5ff2f1deb09fc5fa37b0f4ee08
stansL <mstans8@gmail.com>
2016-07-08 12:50:50 +0300

    URINE FOR HCG tests and positive result count

---
+++ scripts/MOH 706 Laboratory Summary/7. Serology/serology-hcg-positive.sql
@@ -0,0 +1,9 @@
+--URINE FOR HCG - This type of human chorionic gonadotropin (HCG) test measures the specific level of HCG in the
+--urine. HCG is a hormone produced during pregnancy. Thus, this test attempts to determine whether a woman is pregnant.
+--Substitute the dates with date placeholders
+
+SELECT COUNT(*)
+FROM obs
+WHERE obs.concept_id = 2523
+AND obs.value_coded = 782
+AND DATE(obs_datetime) BETWEEN '2016-01-01' AND '2016-06-30';
