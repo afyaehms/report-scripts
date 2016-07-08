813382c7579b15fa4c2b39857ef788abd5fa2d73
stansL <mstans8@gmail.com>
2016-07-08 10:06:47 +0300

    serology  tpha total tests and number positive

---
+++ scripts/MOH 706 Laboratory Summary/7. Serology/serology-tpha-total.sql
@@ -0,0 +1,10 @@
+--TPHA - The Syphilis TPHA test is an indirect hemagglutination assay used for the detection and titration of antibodies
+-- against the causative agent of syphilis, Treponema pallidum subspecies pallidum.
+--Synonyms	:TPHA TEST, TREPONEMA PALLIDUM HAEMAGGLUTINATION TEST
+--Substitute the dates with date placeholders
+--Concept id: 2561
+
+SELECT COUNT(*)
+FROM obs
+WHERE obs.concept_id = 2561
+AND DATE(obs_datetime) BETWEEN '2016-01-01' AND '2016-06-30';
