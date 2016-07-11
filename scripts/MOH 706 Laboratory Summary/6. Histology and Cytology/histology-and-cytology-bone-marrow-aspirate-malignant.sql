dcbff6fc8605cf0bacfa96f8b5f3ed66b67f7467
stansL <mstans8@gmail.com>
2016-07-11 12:26:06 +0300

    scripts for bone marrow aspirate total and mallignant

---
+++ scripts/MOH 706 Laboratory Summary/6. Histology and Cytology/histology-and-cytology-bone-marrow-aspirate-malignant.sql
@@ -0,0 +1,8 @@
+-- BONE MARROW ASPIRATION
+-- Substitute the dates with date placeholders
+
+SELECT COUNT(*) 
+FROM obs o
+WHERE o.concept_id = 2253
+AND o.value_text = 'POSITIVE'
+AND DATE(o.obs_datetime) BETWEEN '2016-07-01' AND '2016-07-31'
