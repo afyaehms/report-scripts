e487f942dd0cccb4a59c1dce7668356642d7fd4a
Margaret Ndisha <mndisha@gmail.com>
2016-07-13 16:17:53 +0300

    removing hard coded dates - removing space <>

--- scripts/MOH 710 Vaccines and Immunisation/measles-doses-remaining-at-the-end-of-the-month.sql
+++ scripts/MOH 710 Vaccines and Immunisation/measles-doses-remaining-at-the-end-of-the-month.sql
@@ -1,14 +1,13 @@
 -- Measles remaining at the end of the month
 -- Substitute the dates with date placeholders
-
 SELECT IFNULL(SUM(closing_balance),0)
-FROM(
-	SELECT closing_balance
-	FROM inventory_store_drug_transaction_detail isd
-	INNER JOIN drug d ON d.drug_id = isd.drug_id
-	WHERE opening_balance <> closing_balance
-	AND d.concept_id = 1020
-	AND DATE(created_on) BETWEEN '2016-06-01' AND '2016-06-30'
-	ORDER BY created_on DESC
-	LIMIT 1
-) AS qnty;
+					FROM(
+						SELECT closing_balance
+						FROM inventory_store_drug_transaction_detail isd
+						INNER JOIN drug d ON d.drug_id = isd.drug_id
+						WHERE opening_balance  &lt;&gt; closing_balance
+						AND d.concept_id = 87
+						AND DATE(created_on) BETWEEN :startOfPeriod AND :endOfPeriod
+						ORDER BY created_on DESC
+						LIMIT 1
+					) AS qnty  
