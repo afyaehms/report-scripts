d1e3b4ef652e0cea6b1bdb5b8cebc71f597e63e6
Margaret Ndisha <mndisha@gmail.com>
2016-07-13 15:48:04 +0300

    removing hard coded dates - removing space <>

--- scripts/MOH 710 Vaccines and Immunisation/bcg-doses-remaining-at-the-end-of-the-month.sql
+++ scripts/MOH 710 Vaccines and Immunisation/bcg-doses-remaining-at-the-end-of-the-month.sql
@@ -1,15 +1,14 @@
 -- BCG remaining at the end of the month
 -- Substitute the dates with date placeholders
 
-
 SELECT IFNULL(SUM(closing_balance),0)
 FROM (
-	SELECT closing_balance
-	FROM inventory_store_drug_transaction_detail isd
-	INNER JOIN drug d ON d.drug_id = isd.drug_id
-	WHERE opening_balance <> closing_balance
-	AND d.concept_id = 193
-	AND DATE(created_on) BETWEEN '2016-06-01' AND '2016-06-30'
-	ORDER BY created_on DESC
-	LIMIT 1
-) AS qnty;
+SELECT closing_balance
+FROM inventory_store_drug_transaction_detail isd
+INNER JOIN drug d ON d.drug_id = isd.drug_id
+WHERE opening_balance  &lt;&gt;  closing_balance
+AND d.concept_id = 193
+AND DATE(created_on) BETWEEN :startOfPeriod AND :endOfPeriod
+ORDER BY created_on DESC
+LIMIT 1
+) AS qnty 
