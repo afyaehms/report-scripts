72f3b38eb4d9a8c6cf96c8838773333ded4c6038
Margaret Ndisha <mndisha@gmail.com>
2016-07-13 15:39:25 +0300

    removing hard coded dates

--- scripts/MOH 710 Vaccines and Immunisation/child-immunization-fully-immunized-children-under-1-year.sql
+++ scripts/MOH 710 Vaccines and Immunisation/child-immunization-fully-immunized-children-under-1-year.sql
@@ -2,20 +2,18 @@
 -- Tests to ensure all vaccination have been administered
 
 SELECT COUNT(*)
-	FROM(
-	SELECT patient_id
-	FROM patient_state ps
-	INNER JOIN patient_program pp ON ps.patient_program_id=pp.patient_program_id
-	INNER JOIN person p ON pp.patient_id = p.person_id
-
-	WHERE birthdate IS NOT NULL 
-	AND (DATEDIFF(CURDATE(),birthdate) / 365) &lt;= 1
-
-	GROUP BY patient_id
-	HAVING COUNT(DISTINCT state)=(
-		SELECT COUNT(program_workflow_state_id)
-		FROM program_workflow_state pws
-		INNER JOIN program_workflow pw 
-		ON pw.program_workflow_id = pws.program_workflow_id AND program_id=3
-	)
-) AS fully_vaccinated;
+FROM(
+SELECT patient_id
+FROM patient_state ps
+INNER JOIN patient_program pp ON ps.patient_program_id=pp.patient_program_id
+INNER JOIN person p ON pp.patient_id = p.person_id
+WHERE birthdate IS NOT NULL 
+AND ((DATEDIFF(:startOfPeriod,birthdate) / 365) &lt;= 1
+OR (DATEDIFF(:endOfPeriod,birthdate) / 365) &lt;= 1)
+GROUP BY patient_id
+HAVING COUNT(DISTINCT state)=(
+SELECT COUNT(program_workflow_state_id)
+FROM program_workflow_state pws
+INNER JOIN program_workflow pw 
+ON pw.program_workflow_id = pws.program_workflow_id AND program_id=3)
+) AS fully_vaccinated 
