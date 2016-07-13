4f8b2e5af02f6762eea46bb16487529362bd318a
Margaret Ndisha <mndisha@gmail.com>
2016-07-13 15:59:15 +0300

    removing hard coded dates - removing space <>

--- scripts/MOH 710 Vaccines and Immunisation/ipv-doses-in-stock-at-the-beggining-of-the-month.sql
+++ scripts/MOH 710 Vaccines and Immunisation/ipv-doses-in-stock-at-the-beggining-of-the-month.sql
@@ -2,13 +2,13 @@
 -- Substitute the dates with date placeholders
 
 SELECT IFNULL(SUM(opening_balance),0)
-FROM(
-	SELECT opening_balance
-	FROM inventory_store_drug_transaction_detail isd
-	INNER JOIN drug d ON d.drug_id = isd.drug_id
-	WHERE opening_balance <> closing_balance
-	AND d.concept_id = 100126152
-	AND DATE(created_on) BETWEEN '2016-06-01' AND '2016-06-30'
-	ORDER BY created_on ASC
-	LIMIT 1
-) AS qnty;
+				FROM(
+					SELECT opening_balance
+					FROM inventory_store_drug_transaction_detail isd
+					INNER JOIN drug d ON d.drug_id = isd.drug_id
+					WHERE opening_balance  &lt;&gt;  closing_balance
+					AND d.concept_id = 100126152
+					AND DATE(created_on) BETWEEN :startOfPeriod AND :endOfPeriod
+					ORDER BY created_on ASC
+					LIMIT 1
+				) AS qnty
