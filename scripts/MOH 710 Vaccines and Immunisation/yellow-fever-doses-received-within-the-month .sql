f0d254afbda8efe3b07f31963cde567947263355
Margaret Ndisha <mndisha@gmail.com>
2016-07-13 16:20:23 +0300

    removing hard coded dates - removing space <>

--- scripts/MOH 710 Vaccines and Immunisation/yellow-fever-doses-received-within-the-month .sql
+++ scripts/MOH 710 Vaccines and Immunisation/yellow-fever-doses-received-within-the-month .sql
@@ -1,10 +1,9 @@
 -- Yellow fever received within the month
 -- Substitute the dates with date placeholders
 
-
 SELECT IFNULL(SUM(quantity), 0)
-FROM inventory_store_drug_transaction_detail isd
-INNER JOIN drug d ON d.drug_id = isd.drug_id
-WHERE opening_balance <> closing_balance
-AND concept_id = 1018
-AND DATE(created_on) BETWEEN '2016-06-01' AND '2016-06-30';
+					FROM inventory_store_drug_transaction_detail isd
+					INNER JOIN drug d ON d.drug_id = isd.drug_id
+					WHERE opening_balance  &lt;&gt;  closing_balance
+					AND concept_id = 240
+					AND DATE(created_on) BETWEEN :startOfPeriod AND :endOfPeriod  
