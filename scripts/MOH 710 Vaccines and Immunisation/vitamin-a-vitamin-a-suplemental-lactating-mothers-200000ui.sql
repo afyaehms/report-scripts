06779415634e2e216a0bb299a6b9097992e97e9a
Margaret Ndisha <mndisha@gmail.com>
2016-07-13 15:30:44 +0300

    removing hard coded dates

--- scripts/MOH 710 Vaccines and Immunisation/vitamin-a-vitamin-a-suplemental-lactating-mothers-200000ui.sql
+++ scripts/MOH 710 Vaccines and Immunisation/vitamin-a-vitamin-a-suplemental-lactating-mothers-200000ui.sql
@@ -3,11 +3,10 @@
 -- The query ensures that the encounter originated from PNC (encounter_type: 20)
 -- Substitute the dates with date placeholders
 
-SELECT COUNT(*) 
+SELECT COUNT(*)
 FROM obs o
-INNER JOIN encounter e 
-ON e.encounter_id = o.encounter_id 
+INNER JOIN encounter e
+ON e.encounter_id = o.encounter_id
 AND e.encounter_type= 20
-
 WHERE concept_id = 325
-AND DATE(obs_datetime) BETWEEN '2016-01-01' AND '2016-06-30';
+AND DATE(obs_datetime) BETWEEN :startOfPeriod AND :endOfPeriod
