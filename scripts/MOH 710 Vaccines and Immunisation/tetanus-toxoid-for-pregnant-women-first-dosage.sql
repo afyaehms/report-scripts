b88742dbc2a2b26af712ae1906c4e64572ba906e
Margaret Ndisha <mndisha@gmail.com>
2016-07-13 15:19:33 +0300

    removing hard coded dates

--- scripts/MOH 710 Vaccines and Immunisation/tetanus-toxoid-for-pregnant-women-first-dosage.sql
+++ scripts/MOH 710 Vaccines and Immunisation/tetanus-toxoid-for-pregnant-women-first-dosage.sql
@@ -3,12 +3,9 @@
 --Substitute the dates with date placeholders
 
 SELECT COUNT(*)
- 
 FROM patient_state ps
 INNER JOIN patient_program pg ON pg.patient_program_id=ps.patient_program_id
 INNER JOIN person p ON pg.patient_id = p.person_id
 INNER JOIN program_workflow_state pws ON pws.program_workflow_state_id=ps.state
- 
-WHERE  pws.concept_id = 100126174
-AND start_date BETWEEN '2016-01-01' AND '2016-06-30'
-
+WHERE pws.concept_id = 100126174
+AND start_date BETWEEN :startOfPeriod AND :endOfPeriod
