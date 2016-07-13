f03413d9d71ed54a470b899ed3794ed0e7d584ff
Margaret Ndisha <mndisha@gmail.com>
2016-07-13 15:29:15 +0300

    removing hard coded dates

--- scripts/MOH 710 Vaccines and Immunisation/vitamin-a-vitamin-a-200000ui-2-years-to-5years.sql
+++ scripts/MOH 710 Vaccines and Immunisation/vitamin-a-vitamin-a-200000ui-2-years-to-5years.sql
@@ -1,15 +1,13 @@
 -- Vitamin A 2 years to 5 years(200,000 IU) (concept_id: 100126165)
 -- Substitute the dates with date placeholders
 
-SELECT COUNT(*)
-
-FROM patient_state ps
-INNER JOIN patient_program pg ON pg.patient_program_id=ps.patient_program_id
-INNER JOIN person p ON pg.patient_id = p.person_id
-INNER JOIN program_workflow_state pws ON pws.program_workflow_state_id=ps.state
-
-WHERE pws.concept_id = 100126165
-AND start_date BETWEEN '2016-01-01' AND '2016-06-30'
-AND birthdate IS NOT NULL 
-AND (DATEDIFF(CURDATE(),birthdate) / 365) &gt;= 2
-AND (DATEDIFF(CURDATE(),birthdate) / 365) &lt;= 5
+  SELECT COUNT(*)
+  FROM patient_state ps
+  INNER JOIN patient_program pg ON pg.patient_program_id=ps.patient_program_id
+  INNER JOIN person p ON pg.patient_id = p.person_id
+  INNER JOIN program_workflow_state pws ON pws.program_workflow_state_id=ps.state
+  WHERE pws.concept_id = 100126165
+  AND start_date BETWEEN :startOfPeriod AND :endOfPeriod
+  AND birthdate IS NOT NULL
+  AND (DATEDIFF(CURDATE(),birthdate) / 365) &gt;= 2
+  AND (DATEDIFF(CURDATE(),birthdate) / 365) &lt;= 5
