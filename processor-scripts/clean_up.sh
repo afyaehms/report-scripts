805d2689d2a1f071461792aab97ab4ec1d979db8
Geof Wasilwa <geoffreywanjala@gmail.com>
2016-09-14 10:38:09 +0300

    Clean up less than and greater than symbols in scripts

--- processor-scripts/clean_up.sh
+++ processor-scripts/clean_up.sh
@@ -3,14 +3,15 @@
 FILE=$1
 filename=$(basename "$FILE")
 
-echo "Replacing date with placeholders in $filename"
-echo "Removing comments from $filename script"
+echo "Cleaning up $filename. Removing comments, dates, encoding characters"
 
 sed "{ 
   s|\('[0-9][0-9][0-9][0-9]-[0-9][0-9]*-[0-9][0-9]*'\)\(\sAND\)|:startOfPeriod\2|I
   s|\(AND\s\)\('[0-9][0-9][0-9][0-9]-[0-9][0-9]*-[0-9][0-9]*'\)|\1:endOfPeriod|I
   s|\(\&lt;\s\)\('[0-9][0-9][0-9][0-9]-[0-9][0-9]*-[0-9][0-9]*'\)|\1:startOfPeriod|I
   s|\(\&gt;\s\)\('[0-9][0-9][0-9][0-9]-[0-9][0-9]*-[0-9][0-9]*'\)|\1:endOfPeriod|I
+  s|>|\&gt;|I
+  s|<|\&lt;|I
   /^\s*--/d
 }" "$FILE"  > "compiled/$filename"
 
